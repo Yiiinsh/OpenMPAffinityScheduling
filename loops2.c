ea9403068f4f2e7208f41a56b6d1cf4fd8e54c4a
Shaohan Yin <shaohan.yin@gmail.com>
2017-11-28 16:42:47 +0000

    Work queue introduced

--- loops2.c
+++ loops2.c
@@ -1,14 +1,154 @@
 #include <stdio.h>
 #include <math.h>
 
-
 #define N 729
 #define reps 1000 
 #include <omp.h> 
 
+/*
+ * Chunk for affinity scheduling 
+ */
+typedef struct _chunk {
+  int lo; // lower bound
+  int hi; // higher bound
+  int workload; // work load
+} chunk;
+
+/* 
+ * Work queue node
+ */
+typedef struct _work_queue_node {
+  struct _work_queue_node *next; // pointer to next node
+  struct _chunk chunk; // work chunk
+} work_queue_node;
+
+/* 
+ * Work queue
+ */
+typedef struct _work_queue {
+  struct _work_queue_node *front; // front of the queue
+  struct _work_queue_node *rear; // rear of the queue
+  int chunk_size; // current chunk(node) size
+  int workload; // current workload in the queue (total amount of iterations)
+} work_queue;
+
+/* 
+ * Initialize a new empty work queue 
+ * inout : queue, pointer to a work_queue
+ */
+void init_work_queue(work_queue *queue) {
+  if(NULL != queue) {
+    queue->front = NULL;
+    queue->rear = NULL;
+    queue->chunk_size = 0;
+    queue->workload = 0; 
+  } else {
+    fprintf(stderr, "Cannot initialize a new work queue from NULL");
+    exit(-1);
+  }
+}
+
+/* 
+ * Destory a work queue
+ * in : queue, pointer to the queue to be destoried
+ */
+void destroy_work_queue(work_queue *queue) {
+  if(NULL != queue) {
+    while(NULL != queue->front) {
+      work_queue_node *tmp;
+      tmp = queue->front;
+      queue->front = queue->front->next;
+      free(tmp);
+    }
+    queue->rear = queue->front;
+    queue->chunk_size = 0;
+    queue->workload = 0;
+  }
+}
+
+/* 
+ * Enqueue a new chunk
+ * in : queue, pointer to the queue
+ * in : new_chunk, chunk to be added to the queue
+ */
+void enqueue(work_queue *queue, chunk new_chunk) {
+  if(NULL != queue) {
+    work_queue_node *new_node = (work_queue_node *) malloc(sizeof(work_queue_node));
+    new_node->next = NULL;
+    new_node->chunk.lo = new_chunk.lo;
+    new_node->chunk.hi = new_chunk.hi;
+    new_node->chunk.workload = new_chunk.workload;
+
+    if(NULL == queue->front) {
+      queue->front = new_node;
+      queue->rear = new_node;
+    } else {
+      queue->rear->next = new_node;
+      queue->rear = queue->rear->next;
+    }
+    queue->workload += new_node->chunk.workload;
+    ++queue->chunk_size;
+  } else {
+    fprintf(stderr, "Cannot enqueue to a NULL queue");
+  }
+}
+
+/* 
+ * Dequeue from queue
+ * in : queue, pointer to the queue
+ * return : front element of the queue, return NULL if queue is empty
+ */
+chunk dequeue(work_queue *queue) {
+  chunk new_chunk;
+  new_chunk.lo = 0;
+  new_chunk.hi = 0;
+  new_chunk.workload = 0;
+
+  if(NULL != queue) {
+    if(NULL != queue->front) {
+      work_queue_node *tmp_node = queue->front;
+      queue->front = queue->front->next;
+      queue->workload -= tmp_node->chunk.workload;
+      --queue->chunk_size;
+
+      new_chunk.lo = tmp_node->chunk.lo;
+      new_chunk.hi = tmp_node->chunk.hi;
+      new_chunk.workload = tmp_node->chunk.workload;
+
+      free(tmp_node);
+    }
+  }
+  return new_chunk;
+}
+
+/* 
+ * Print information about the work queue
+ * in : queue, pointer to the queue
+ */
+void print_queue(work_queue *queue) {
+  if(NULL != queue) {
+    work_queue_node *node = queue->front;
+    while(NULL != node) {
+      printf("[%d,%d) ", node->chunk.lo, node->chunk.hi);
+      node = node->next;
+    }
+    printf("\n");
+  }
+}
+
+/* 
+ * Check if the queue is empty
+ * in : queue, pointer to the queue
+ * return : flag represents if the queue is empty
+ */
+int is_queue_empty(work_queue *queue) {
+  if(NULL != queue) {
+    return (queue->chunk_size == 0) ? 1 : 0;
+  }
+}
+
 double a[N][N], b[N][N], c[N];
 int jmax[N];  
-
 
 void init1(void);
 void init2(void);
@@ -17,7 +157,6 @@ void loop1chunk(int, int);
 void loop2chunk(int, int);
 void valid1(void);
 void valid2(void);
-
 
 int main(int argc, char *argv[]) { 
 
@@ -89,22 +228,72 @@ void init2(void){
  
 } 
 
-
 void runloop(int loopid)  {
-
-#pragma omp parallel default(none) shared(loopid) 
+  work_queue *work_queues;
+  omp_lock_t *work_queue_locks;
+#pragma omp parallel default(none) shared(loopid, work_queues, work_queue_locks)
   {
     int myid  = omp_get_thread_num();
     int nthreads = omp_get_num_threads(); 
-    int ipt = (int) ceil((double)N/(double)nthreads); 
-    int lo = myid*ipt;
-    int hi = (myid+1)*ipt;
-    if (hi > N) hi = N; 
-  
-    switch (loopid) { 
-       case 1: loop1chunk(lo,hi); break;
-       case 2: loop2chunk(lo,hi); break;
-    } 
+
+    /* Work breakdown */
+#pragma omp single
+    {
+      /* Init work queues and locks */
+      work_queues = (work_queue *) malloc(nthreads * sizeof(work_queue));
+      work_queue_locks = (omp_lock_t *) malloc(nthreads * sizeof(omp_lock_t));
+      for(int i = 0; i < nthreads; ++i) {
+        init_work_queue(&work_queues[i]);
+        omp_init_lock(&work_queue_locks[i]);
+      }
+    }
+
+    /* Local work set break down */
+    int ipt = (int) ceil((double) N / (double) nthreads);
+    int local_work_set_start = myid * ipt;
+    int local_work_set_end = (myid + 1) * ipt;
+    local_work_set_end = (local_work_set_end > N) ? N : local_work_set_end;
+    
+    while(local_work_set_start < local_work_set_end) {
+      int workload = (int) ceil((double)(local_work_set_end - local_work_set_start) / (double)nthreads);
+      chunk new_chunk;
+      new_chunk.lo = local_work_set_start;
+      new_chunk.hi = local_work_set_start + workload;
+      new_chunk.workload = workload;
+
+      /* Fill in thread corresponding work queue */
+      enqueue(&work_queues[myid], new_chunk);
+
+      local_work_set_start = local_work_set_start + workload;
+    }
+#pragma omp barrier
+
+    /* Local work execution */
+    while(! is_queue_empty(&work_queues[myid])) {
+      chunk current_work;
+      omp_set_lock(&work_queue_locks[myid]);
+      current_work = dequeue(&work_queues[myid]);
+      omp_unset_lock(&work_queue_locks[myid]);
+
+      switch(loopid) {
+        case 1: loop1chunk(current_work.lo,current_work.hi); break;
+        case 2: loop2chunk(current_work.lo,current_work.hi); break;
+      }
+    }
+
+    /* Work stealing */
+
+
+    /* Resource release */
+    destroy_work_queue(&work_queues[myid]);
+    omp_destroy_lock(&work_queue_locks[myid]);
+  }
+
+  if(NULL != work_queues) {
+    free(work_queues);
+  }
+  if(NULL != work_queue_locks) {
+    free(work_queue_locks);
   }
 }
 
@@ -118,8 +307,6 @@ void loop1chunk(int lo, int hi) {
   }
 
 } 
-
-
 
 void loop2chunk(int lo, int hi) {
   int i,j,k; 
@@ -150,7 +337,6 @@ void valid1(void) {
   printf("Loop 1 check: Sum of a is %lf\n", suma);
 
 } 
-
 
 void valid2(void) { 
   int i; 
